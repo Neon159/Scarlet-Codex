d16a48efae3eedca200b72c27b4272b7508c126c
Neon159 <73184558+Neon159@users.noreply.github.com>
2021-07-11 20:06:02 -0500

    Se agrega el Proyecto

---
+++ js/cursos.js
@@ -0,0 +1,12 @@
+export class Cursos {
+    constructor(NombreClase,URLI,URL,DescripcionClase) {
+        // this.nombreCurso = nombreCurso;
+        // this.NombreDocente = NombreDocente;
+        // this.DescripcionCurso= DescripcionCurso;
+        
+        this.NombreClase = NombreClase;
+        this.URLI=URLI;
+        this.URL = URL;
+        this.DescripcionClase = DescripcionClase;
+    }
+}
